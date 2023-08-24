0a7aeb2ff231ad9110249b8ac8a24de3d90ceb30
roseirungu23 <rosemarywanjikui2016@gmail.com>
2023-08-24 01:02:07 -0700

    add opcode

---
+++ add_opcode.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+/**
+ * add - the opcode adds the top two elements of the stack
+ * @stack: The linear  data structure.
+ * @line_number: The line number.
+ * Return: Void.
+ */
+
+void add(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL || (*stac)->next == NULL)
+	{
+		fprint(stderr, "L%u: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	(*stack)->next->n += (*stack)->n;
+	pop(stack, line_number);
+}
