b2fac7825ce375a7998ffd9e8175b51dbb28c3f9
LyneaW <wangecilynea@gmail.com>
2023-08-24 01:03:07 -0700

    Swap File

---
+++ swap_opcode.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+/**
+ * swap - The opcode swap swaps the top two elements of the stack.
+ * @stack: Linear data structure.
+ * @line_number: The line number.
+ * Return: Nothing.
+ */
+
+void swap(stack_t **stack, unsigned int line_number)
+{
+int temp = (*stack)->n;
+if (is_stack_empty(*stack) || !(*stack)->next)
+{
+fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+exit(EXIT_FAILURE);
+}
+(*stack)->n = (*stack)->next->n;
+(*stack)->next->n = temp;
+}
